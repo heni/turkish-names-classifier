95e593e84a78cd33f98fc944fc5982df6084508b
heni <eugene.krokhalev@gmail.com>
2015-07-28 08:59:10 +0300

    Adds data extracting script

---
+++ extract-data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+import re
+import icu
+import gzip
+import json
+import collections
+
+def LoadData(filename):
+    TurLocale = icu.Locale('tr')
+    reFormat = re.compile(u"^([^:]+):\s*(?:\(([^()]+\.)\))?\s*([^-1()]*)\s*(?:-|—|–|(?=1\.))(.*)$", re.U)
+    reUnisex = re.compile(u"Erkek ve(ya)? (kadın|kız) (adı|ismi) olarak kullan(ılır|ılabilir)", re.U)
+    reOptsDelim = re.compile("[.\s]")
+    with open(filename) as data_reader:
+        for ln in data_reader:
+            ln = ln.strip().decode("utf-8")
+            if ln:
+                sRes = reFormat.match(ln)
+                name, nameTp, nameOpts, desc = sRes.groups()
+                name = unicode(icu.UnicodeString(name).toLower(TurLocale)).strip()
+                nameOpts = set(opt.strip() for opt in reOptsDelim.split(nameOpts) if opt.strip())
+                if reUnisex.search(desc):
+                    nameOpts.add(u'Er')
+                    nameOpts.add(u'Ka')
+                yield collections.OrderedDict([
+                    ("name", name),
+                    ("type", nameTp),
+                    ("gender", list(nameOpts)),
+                    ("description", desc),
+                ])
+
+
+if __name__ == "__main__":
+    data = list(LoadData("data/turk-names.txt"))
+    with gzip.open("data/turk-names.js.gz", "w") as names_prn:
+        names_prn.write(json.dumps(data, indent=2, ensure_ascii=False).encode("utf-8"))
+
